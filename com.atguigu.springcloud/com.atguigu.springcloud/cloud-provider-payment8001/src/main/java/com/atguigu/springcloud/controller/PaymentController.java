180902912961e0c67f956a247e8815589d0cb930
Bing Yang (bingy) <bingy@cisco.com>
2021-01-14 14:07:39 +0800

    learn code

--- com.atguigu.springcloud/com.atguigu.springcloud/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ com.atguigu.springcloud/com.atguigu.springcloud/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -4,10 +4,8 @@ import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -18,12 +16,15 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @PostMapping(value = "/payment/create")
-    public CommonResult create(Payment payment){
+    public CommonResult create(@RequestBody Payment payment){
         int result = paymentService.create(payment);
         log.info("*****插入结果： " + result);
         if(result > 0){
-            return new CommonResult(200, " insert success", result);
+            return new CommonResult(200, " insert success", result + " serverPort " + serverPort);
         } else {
             return new CommonResult(444, "insert fail");
         }
@@ -34,7 +35,7 @@ public class PaymentController {
         Payment payment = paymentService.getPaymentById(id);
         log.info("*****查询结果： " + payment);
         if(payment != null){
-            return new CommonResult(200, "query success", payment);
+            return new CommonResult(200, "query success", payment + " serverPort: " + serverPort);
         } else {
             return new CommonResult(444, "query no result for id: " + id);
         }
